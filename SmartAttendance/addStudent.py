7b2b70432aaf860f6a03ae026d37c92fa0fe7753
Shivam Singhal <44027236+shivamsinghal16@users.noreply.github.com>
2019-04-05 00:24:34 +0530

    Add Student Details

---
+++ SmartAttendance/addStudent.py
@@ -0,0 +1,71 @@
+import os
+import sys
+import cv2
+import sqlite3
+
+name = input("Enter Student Name: ")
+rollNo = int(input("Enter University Roll No.: "))
+sectionRollNo = int(input("Enter Section Roll No.: "))
+section = input("Enter Section: ")
+
+students = os.listdir("Database/{}".format(section))
+
+if str(rollNo) in students:
+    print("Student is already added")
+    sys.exit()
+else:
+    os.mkdir("Database/{}/{}".format(section,rollNo))
+    databasePath = "Database/{}/{}".format(section,rollNo)
+
+def addToDatabase(rollNo, sectionRollNo, name):
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+    cursor.execute('INSERT INTO students values(?,?,?)',(rollNo,sectionRollNo,name))
+    connection.commit()
+    connection.close()
+
+addToDatabase(rollNo,sectionRollNo,name)
+
+cascPath = "haarcascades/haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+capture = cv2.VideoCapture(0)
+
+count = 0
+
+def cropFace(image):
+    global count
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
+    for (x,y,w,h) in faces:
+        count += 1
+        #cv2.rectangle(image, (x, y), (x+w, y+h), (255,255,255), 2)
+        sub_face = image[y-20:y+h+20, x-20:x+w+20]
+        faceFileName = "{}/face".format(databasePath) + str(count) + ".jpg"
+        cv2.imwrite(faceFileName, sub_face)
+
+while(True):
+    ret, frame = capture.read()
+
+    cv2.imshow('Frame', frame)
+
+    k = cv2.waitKey(1)
+
+    if k == ord('c'):
+        if count<25:
+            cropFace(frame)
+        else:
+            print("{} added Successfully".format(rollNo))
+            break
+
+capture.release()
+cv2.destroyAllWindows()
